cc0a3bde817a3546af0c5cc55919df98cd9c3fdd
stray-code <abc1007020@gmail.com>
2024-04-06 10:01:09 +0900

    crxjs追加と設定

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "全URLコピー",
+  version: "1.0.0",
+})
+
+export default defineConfig({
+  plugins: [crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+})
