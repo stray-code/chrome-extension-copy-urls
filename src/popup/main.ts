d88ae727f645a5cba26aba1e7779955c44c18776
stray-code <abc1007020@gmail.com>
2024-04-06 11:41:23 +0900

    タブのコピー機能作成

---
+++ src/popup/main.ts
@@ -0,0 +1,65 @@
+import van from "vanjs-core";
+
+import "./style.css";
+
+const App = () => {
+  const { div, button } = van.tags;
+
+  return div(
+    button(
+      {
+        type: "button",
+        onclick: async () => {
+          const tabs = await chrome.tabs.query({
+            currentWindow: true,
+          });
+
+          const urlsText = tabs.map((tab) => tab.url).join("\n");
+
+          await navigator.clipboard.writeText(urlsText);
+
+          window.close();
+        },
+      },
+      "全てのタブのURLをコピー",
+    ),
+    button(
+      {
+        type: "button",
+        onclick: async () => {
+          const tabs = await chrome.tabs.query({
+            currentWindow: true,
+          });
+
+          const titlesText = tabs.map((tab) => tab.title).join("\n");
+
+          await navigator.clipboard.writeText(titlesText);
+
+          window.close();
+        },
+      },
+      "全てのタブのタイトルをコピー",
+    ),
+    button(
+      {
+        type: "button",
+        onclick: async () => {
+          const tabs = await chrome.tabs.query({
+            currentWindow: true,
+          });
+
+          const titlesText = tabs
+            .flatMap((tab) => [tab.title, tab.url])
+            .join("\n");
+
+          await navigator.clipboard.writeText(titlesText);
+
+          window.close();
+        },
+      },
+      "全てのタブのタイトルとURLをコピー",
+    ),
+  );
+};
+
+van.add(document.getElementById("app")!, App());
