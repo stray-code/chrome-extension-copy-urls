1de8e0c06cc664e3e5a32e804db04cb6f64aafd2
stray-code <abc1007020@gmail.com>
2024-04-10 23:32:33 +0900

    バージョン更新

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ const manifest = defineManifest({
   manifest_version: 3,
   name: "全てのタブのURLをコピー",
   description: "全てのタブのURLをコピーします。",
-  version: "1.0.0",
+  version: "1.0.1",
   icons: {
     16: "img/icon16.png",
     48: "img/icon48.png",
